9b1f62ac3263211533534d15db520ff8909cd4cf
romannjoroge <roman.njoroge@njuguna.com>
2024-12-28 00:23:52 +0300

    feat: Checking player and method

--- backend/src/read/transactions/main.ts
+++ backend/src/read/transactions/main.ts
@@ -1,3 +1,4 @@
+import { decodeTransactionInput } from "./decode-transaction";
 import { getBlockNumber, getTransactions } from "./rpc";
 import "dotenv/config";
 
@@ -6,11 +7,22 @@ async function main() {
         const startBlock = process.env.BLOCK_NUMBER;
         const resp = await getTransactions(startBlock, "0xD4a3f1D145C185E222cCC03D60A1D9fB5e0c2048");
 
-        const player = ("0x9e1c54Bd65Ed82C81aB4C25Cdeba81E76F4fb064").toLowerCase();
+        const player = ("0x139e7542fefc910bb19e73c0dce00c675361c68d").toLowerCase();
+        const playerAddressVariable = "operatorWallet"
+        const functionName = "startAndEndValidatedDungeonBattle"
         for (let transaction of resp.result) {
-            if (transaction.from === player) {
-                console.log("Player Transaction");
-                console.log(transaction);
+            // Decode transaction data
+            const decoded = decodeTransactionInput(transaction.input)
+
+            // Get if transaction is of the right method
+            const method = (decoded["__method__"])as string
+            if(method.includes(functionName)) {
+                // Get transaction for a certain player
+                const decodedPlayer = (decoded[playerAddressVariable]) as string
+                if (decodedPlayer.toLowerCase() === player) {
+                    console.log("Player Transaction");
+                    console.log(transaction)
+                }
             }
         }
     } catch(err) {
