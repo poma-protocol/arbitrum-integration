4dfa37be1c9bd990ef00bf854fd91a0dcb3db127
romannjoroge <roman.njoroge@njuguna.com>
2024-12-27 11:57:49 +0300

    feat: Arbitrum version

---
+++ backend/process-env.d.ts
@@ -0,0 +1,9 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            ARIBTRUM_NOVA_KEY: string
+        }
+    }
+}
+
+export {}
--- backend/src/read/transactions/main.ts
+++ backend/src/read/transactions/main.ts
@@ -1,51 +1,40 @@
-import { Web3 } from "web3";
+import { Alchemy, Network } from "alchemy-sdk";
+import "dotenv/config";
 
-const web3 = new Web3('wss://ethereum-rpc.publicnode.com');
+const settings = {
+    apiKey: process.env.ARIBTRUM_NOVA_KEY,
+    network: Network.ARBNOVA_MAINNET
+}
 
 async function main() {
-    // Listening for every new block
-    const subscription = await web3.eth.subscribe('newBlockHeaders');
-    subscription.on('data', async (data) => {
-        // Assume that when a new block is created it means that the previous block has been finalized so we can process its transactions
-        if (data.number) {
-            if (typeof data.number === "bigint" || typeof data.number === "number") {
-                let prevBlockNum: number | bigint
-                if (typeof data.number === "number") {
-                    prevBlockNum = data.number - 1;
-                } else {
-                    prevBlockNum = data.number - 1n;
-                }
+    const alchemy = new Alchemy(settings);
 
-                try {
-                    const blockToProcess = await web3.eth.getBlock(prevBlockNum, true);
-                    const transactions = blockToProcess.transactions;
-                    console.log("Block Number =>", prevBlockNum);
-                    
-                    // For now test with USDC
-                    for (const transaction of transactions) {
-                        if (typeof transaction !== "string") {
-                            // Check to account to see if it matches the game contract
-                            if (transaction.to === "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".toLowerCase()) {
-                                console.log("USDC transaction");
-                                console.log(transaction);
-                                // Check from account to see if its for a player using Poma
-                            }
-                        } else {
-                            console.log("Transaction is just a string!", transaction);
-                        }
+    let blockNumber = await alchemy.core.getBlockNumber();
+    while (true) {
+        // Get block number
+        let newBlockNumber = await alchemy.core.getBlockNumber();
+        if (newBlockNumber !== blockNumber) {
+            blockNumber = newBlockNumber
+
+            console.log("Block Number =>", blockNumber);
+
+            // Get transactions of block
+            const block = await alchemy.core.getBlock(blockNumber);
+
+            for (let t of block.transactions) {
+                const transaction = await alchemy.core.getTransaction(t);
+                if (transaction) {
+                    // Check to account to see if it matches the game contract
+                    if (transaction.to === "0x00000000000000000000000000000000000A4B05") {
+                        console.log("USDC transaction");
+                        console.log(transaction);
+                        // Check method being called
                     }
-                    
-
-                } catch(err) {
-                    console.log("Error Processing Block =>", err);
                 }
-            } else {
-                console.log("Block number is an enum!", data.number);
+
             }
-        } else {
-            console.log("Data has no number!");
         }
-    });
+    }
 }
 
 main();
