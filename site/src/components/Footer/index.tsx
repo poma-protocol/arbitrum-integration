23f12df0d698ff9a3cd560228c7dfa8591a47854
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-12-30 13:06:57 +0300

    feat: shadncn setup

---
+++ site/src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function Footer(){
+    return (
+        <div className="w-full bottom-0">
+            The whole footer
+        </div>
+    )
+}
---
+++ site/src/components/Navbar/index.tsx
@@ -0,0 +1,6 @@
+"use client"
+export default function Navbar() {
+    return (<div className="w-full">
+        The Navbar
+    </div>)
+}
