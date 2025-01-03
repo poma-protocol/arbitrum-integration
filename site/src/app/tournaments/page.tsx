aeea73cbeee9265b7b42e69b5f5543bcaac5b323
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2025-01-03 15:50:38 +0300

    feat: fetch games from backend

--- site/src/app/tournaments/page.tsx
+++ site/src/app/tournaments/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { toast } from "sonner";
 interface Tournament {
     id: number;
     title: string;
