c831c6714d9b79b8573958c64ec7f3f3b052eb38
romannjoroge <roman.njoroge@njuguna.com>
2025-02-24 11:24:05 +0300

    feat: Add participant smart contract call has been updated

--- backend/src/smartcontract/index.ts
+++ backend/src/smartcontract/index.ts
@@ -1,6 +1,7 @@
 import Web3, { ContractOnceRequiresCallbackError } from "web3";
 import { account, contract, web3 } from "./account";
 import { Errors, MyError } from "../helpers/errors";
+import "dotenv/config";
 
 export class SmarContract {
     web3: Web3
@@ -47,16 +48,26 @@ export class SmarContract {
         }
     }
 
-    async addParticipant(activityID: number, address: string) {
+    async addParticipant(activityID: number, address: string): Promise<string> {
         try {
-            await contract.methods.addParticipant(
-                BigInt(activityID), 
-                address
-            ).send({
+            const block = await web3.eth.getBlock();
+            const transaction = {
                 from: account.address,
-                gas: '1000000',
-                gasPrice: "10000000000"
-            })
+                to: process.env.CONTRACT,
+                data: contract.methods.addParticipant(
+                    BigInt(activityID), 
+                    address
+                ).encodeABI(),
+                maxFeePerGas: block.baseFeePerGas! * 2n,
+                maxPriorityFeePerGas: 100000,
+            };
+
+            const signedTransaction = await web3.eth.accounts.signTransaction(
+                transaction,
+                account.privateKey,
+            );
+            const receipt = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+            return receipt.transactionHash.toString();
         } catch(err) {
             console.log("Error Adding Participant =>", err);
             throw new MyError(Errors.NOT_ADD_PARTICIPANT);
