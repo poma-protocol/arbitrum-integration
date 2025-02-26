fe2ff7dce3aeb3a5981ca4974a716216e21c1e36
romannjoroge <roman.njoroge@njuguna.com>
2025-02-26 09:02:04 +0300

    feat: Setting up Jest tests

---
+++ backend/jest.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+    clearMocks: true,
+    testEnvironment: "node",
+    transform: {
+      "^.+.tsx?$": ["ts-jest",{}],
+    },
+  };
+  
