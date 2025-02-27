7d07c1c83b68cc18c44d92f5c0fe725020b67b0c
romannjoroge <roman.njoroge@njuguna.com>
2025-02-27 09:50:10 +0300

    chore: Adding a type for app to remove error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import Express from "express";
 import router from "./routes";
 import cors from "cors";
 
-const app = Express();
+const app: Express.Express = Express();
 app.use("/", cors());
 app.use("/", Express.json());
 app.use("/game", router.game);
