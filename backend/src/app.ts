e5ad3c7d8248772b7c5f06a41ab1f5cbf5d2ba4a
romannjoroge <roman.njoroge@njuguna.com>
2025-02-28 08:49:05 +0300

    feat: Create jackpot route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ const app: Express.Express = Express();
 app.use("/", cors());
 app.use("/", Express.json());
 app.use("/game", router.game);
-app.use("/activity", router.activity)
+app.use("/activity", router.activity);
+app.use("/jackpot", router.jackpot);
 
 export default app;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import game from "./game";
 import activity from "./activity";
+import jackpot from "./jackpot";
 
 const routes = {
     game,
-    activity
+    activity,
+    jackpot
 }
 
 export default routes;
