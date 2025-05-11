9bd86c169e7904d15246e37df11d263980da0913
Roman Njoroge <roman.njoroge@njuguna.com>
2025-05-11 18:43:18 +0300

    chore: Formatting

--- backend/src/read/transactions/decode-transaction.ts
+++ backend/src/read/transactions/decode-transaction.ts
@@ -1,15 +1,15 @@
-import {ContractAbi, Web3} from "web3";
+import { ContractAbi, Web3 } from "web3";
 import "dotenv/config";
 
 export function decodeTransactionInput(input: string, ABI: ContractAbi, contractAddress: string): Record<string, any> {
     try {
         const web3 = new Web3();
         const contract = new web3.eth.Contract(ABI, contractAddress);
-        
+
         // Decode transaction input
         const result = contract.decodeMethodData(input);
         return result;
-    } catch(err) {
+    } catch (err) {
         console.log("Error Decoding Transaction =>", err);
         throw new Error("Could Not Decode Transaction");
     }
